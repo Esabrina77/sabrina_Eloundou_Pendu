29f110db0892f0d9427d80660ca27b5f51ce8252
Esabrina77 <146923436+Esabrina77@users.noreply.github.com>
2023-11-06 09:59:17 +0100

    bonjour

--- main.go
+++ main.go
@@ -8,11 +8,10 @@ import (
 	"os"
 	"strings"
 	"time"
-	"unicode/utf8"
 )
 
 func main() {
-	/* 
+
 	//Ouverture du fichier
 	file, err := os.Open("mots.txt")
 	if err != nil {
@@ -44,7 +43,6 @@ func main() {
 
 	var choix int
 	fmt.Scanln(&choix)
-
 	var mot string
 	switch choix {
 	case 1:
@@ -60,19 +58,20 @@ func main() {
 		return
 	}
 
-	vie := 8
+	vie := 9
 	Score := 0
 	Punition := 2
 	lettresDevinees := make([]bool, len(mot))
 	LettreAleatoires(lettresDevinees)
 	lettresDevinees[0] = true
+	lettresProposees := make(map[string]bool)
 
+	// OUVRIR LE FICHIER DU BONHOMME PENDU
 	penduFile, err := os.Open("Hangman.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer penduFile.Close()
-
 	var pendu []string
 	scanner = bufio.NewScanner(penduFile)
 	bloc := ""
@@ -90,39 +89,97 @@ func main() {
 		penduInverse = append(penduInverse, pendu[i])
 	}
 	pendu = penduInverse
-
+	reader := bufio.NewReader(os.Stdin)
 	for vie > 0 {
 		fmt.Println()
 		fmt.Println("Mot : ", afficherMot(mot, lettresDevinees))
 		fmt.Println("Vies restantes : ", vie)
 		fmt.Println("Entrez une lettre : ")
-		var lettre string
-		fmt.Scanln(&lettre) */
-b 
-   input, err := reader.ReadString('\n')
-     if err!= nil{
-		log.Fatal(err)
-	 }
- input= strings.TrimSpace(input)
- if len(input)==1 
-   lettre:= strings.Tolower(input)
 
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
 
-      if lettreDejaProposee( lettre, lettresProposees){
-	   fmt.Println("lettre déjà proposée. Veuillez saisir une nouvelle lettre.")
-	      continue 
-	  }
-	  lettresProposees[lettre]=true
+		if len(input) == 0 {
+			fmt.Println(" Veuillez Entrez une lettre !! ")
+			continue
+		}
+
+		if len(input) == 1 {
+			lettre := strings.ToLower(input)
+			if lettreDejaProposee(lettre, lettresProposees) {
+				fmt.Println("lettre déjà proposée. Veuillez saisir une nouvelle lettre.")
+				continue
+			}
+
+			if strings.Contains(mot, lettre) {
+				lettresProposees[lettre] = true
+				for i, l := range mot {
+					if string(l) == lettre {
+						lettresDevinees[i] = true
+					}
+				}
+
+				fmt.Println()
+				fmt.Println("lettre correcte")
+				Score++
+
+			} else {
+				fmt.Println("lettre incorrecte")
+				vie--
+				afficherPendu(pendu, vie)
+			}
+
+		} else if len(input) > 1 {
+			if input == mot {
+				lettresDevinees = make([]bool, len(mot))
+
+				fmt.Println()
+				fmt.Println("Félicitations! Vous avez deviné le mot:", mot)
+				Score += 10
+				fmt.Println()
+				fmt.Println("Votre score FINAL est :", Score)
+				return
+			} else {
+				vie -= Punition
+				Score -= 2
+				if Score < 0 {
+					Score = 0
+				}
+				afficherPendu(pendu, vie)
+				fmt.Println("LE MOT PRPOPOSE EST INCORRECT!!")
+			}
+		}
+
+		if motComplet(lettresDevinees) {
+			fmt.Println()
+			fmt.Println("Bien joué! vous avez deviné le mot:", mot)
+
+			break
+		}
+
+		if vie == 0 {
+			fmt.Println()
+			fmt.Println("Vous avez perdu! Le mot était:", mot)
+			return
+		}
+		fmt.Println("Votre score actuel est :", Score)
 	}
-
-
-
-func lettreDejaProposee(lettre string, lettresProposees map[string]bool) bool{
-	_,OK := lettresProposees[lettre]
-	return OK
+	fmt.Println("Votre score FINAL est :", Score)
+	fmt.Println()
 }
 
-/* 
+func lettreDejaProposee(lettre string, lettresProposees map[string]bool) bool {
+	for k := range lettresProposees {
+		if strings.ContainsRune(k, []rune(lettre)[0]) {
+			return true
+		}
+	}
+	return false
+}
+
 func choisirMot(mots []string, minLen, maxLen int) string {
 	var motsFiltres []string
 	for _, mot := range mots {
@@ -172,16 +229,6 @@ func afficherPendu(pendu []string, vie int) {
 		fmt.Println(pendu[len(pendu)-1])
 	}
 }
-func contientLettre(mot, lettre string) bool {
-	for len(mot) > 0 {
-		r, size := utf8.DecodeRuneInString(mot)
-		if strings.ToLower(string(r)) == strings.ToLower(lettre) {
-			return true
-		}
-		mot = mot[size:]
-	}
-	return false
-}
 
 func mettreAJourLettresDevinees(mot, lettre string, lettresDevinees []bool) {
 	lettre = strings.ToLower(lettre)
@@ -191,4 +238,3 @@ func mettreAJourLettresDevinees(mot, lettre string, lettresDevinees []bool) {
 		}
 	}
 }
-*/
